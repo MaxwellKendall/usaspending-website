f45d29ba1894d1ef3ea2b10009e94b521ffc3680
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-01-12 10:42:42 -0500

    [DEV-6609] Fixing Tests to use new props
    1. CovidHighlights -- now accepts prop submissionPeriods[]
    2. AccountTimePeriodContainer -- now accepts latestPeriod{year: 2020}
    3. TimeFilters -- (a) no longer mocking network request, instead we're
    mocking the custom hook that makes the request;
    (b) so we dont need waitForElementToBeRemoved
    4. AccountLandingContainer -- (a) this.props.fy >>
    this.props.latestPeriod.year; (b) column name updated to match prod
    (remove prepended $)
    5. AccountContainer -- adding props: submissionPeriods and latestPeriod

--- tests/components/aboutTheData/TimeFilters-test.jsx
+++ tests/components/aboutTheData/TimeFilters-test.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Set } from 'immutable';
+import { render, screen } from '@test-utils';
 
-import { render, screen, waitForElementToBeRemoved } from '@test-utils';
 import TimeFilters from 'components/aboutTheData/TimeFilters';
+import * as withLatestAccount from "containers/account/WithLatestFy";
 import * as accountHelpers from 'helpers/accountHelper';
 import * as glossaryHelpers from 'helpers/glossaryHelper';
 import { mockGlossary, mockSubmissions } from '../../mockData';
@@ -19,6 +21,8 @@ const q4Fy2020 = {
     period_start_date: "2020-09-01T00:00:00Z", period_end_date: "2020-09-30T00:00:00Z", submission_start_date: "2020-10-19T00:00:00Z", submission_due_date: "2020-11-17T00:00:00Z", certification_due_date: "2020-11-17T00:00:00Z", submission_reveal_date: "2020-11-17T00:00:00Z", submission_fiscal_year: 2020, submission_fiscal_quarter: 4, submission_fiscal_month: 12, is_quarter: false
 };
 
+const periodsAsSet = new Set([...mockSubmissions, q4Fy2020]);
+
 beforeEach(() => {
     jest.spyOn(accountHelpers, 'fetchAllSubmissionDates').mockReturnValue({
         promise: Promise.resolve({ data: { available_periods: [...mockSubmissions, q4Fy2020] } }),
@@ -26,6 +30,11 @@ beforeEach(() => {
             console.log('cancel called');
         }
     });
+    jest.spyOn(withLatestAccount, "useLatestAccountData").mockReturnValue([
+        accountHelpers.getLatestPeriodAsMoment(periodsAsSet.toJS()),
+        periodsAsSet,
+        accountHelpers.getLatestPeriod(periodsAsSet.toJS())
+    ]);
     jest.spyOn(glossaryHelpers, 'fetchAllTerms').mockReturnValue({
         promise: Promise.resolve(mockGlossary),
         cancel: () => {
@@ -48,13 +57,11 @@ test('Validates fiscal year from url', async () => {
     // bad fiscal year:
     const mockOnTimeSelection = jest.fn();
     render(<TimeFilters {...defaultProps} onTimeFilterSelection={mockOnTimeSelection} urlFy="1990" />);
-    await waitForElementToBeRemoved(screen.queryByText('Loading fiscal years...'));
     expect(mockOnTimeSelection).toHaveBeenCalledWith("2020", { className: "last-period-per-quarter", id: "12", title: "Q4 P12" });
 });
 
 test('Validates period from url', async () => {
     const mockOnTimeSelection = jest.fn();
     render(<TimeFilters {...defaultProps} onTimeFilterSelection={mockOnTimeSelection} urlFy="2020" urlPeriod="13" />);
-    await waitForElementToBeRemoved(screen.queryByText('Loading fiscal years...'));
     expect(mockOnTimeSelection).toHaveBeenCalledWith("2020", { className: "last-period-per-quarter", id: "12", title: "Q4 P12" });
 });
